e636af280cfa9729821a2698d724572b51a97317
kureida <kureida@gmail.com>
2015-09-11 21:04:28 -0400

    initial commit

---
+++ PoeApp/PoeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoeApp.Startup))]
+namespace PoeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
