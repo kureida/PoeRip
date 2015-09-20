645cbdc35854c0a8206e2792abeb9f799e592d43
Eric <ericmun22@gmail.com>
2015-09-20 18:30:36 -0400

    hueg changes

---
+++ PoeApp/PoeApp/PoeRESTService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace PoeApp
+{
+    public class PoeRESTService
+    {
+        readonly string uri = "http://api.pathofexile.com/ladders/Tempest?limit=20";
+
+        public LadderData GetLadder()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                return JsonConvert.DeserializeObject<LadderData>(webClient.DownloadString(uri));
+            }
+        }
+
+        public async Task<LadderData> GetLadderAsync()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                return JsonConvert.DeserializeObject<LadderData>(await httpClient.GetStringAsync(uri));
+            }
+        }
+    }
+}
